964bb7f9e07f062ba2c7ff24d624916366ec6ab2
Guillermo Misa <guillermo_misa@vfc.com>
2022-01-02 23:27:02 -0800

    theme app extensions

--- theme-app-extension/assets/social-commerce-init.js
+++ theme-app-extension/assets/social-commerce-init.js
@@ -1,4 +1,7 @@
-<script>
-var CRL8_SITENAME = 'Habiliment-RUQGBj'; 
+var CRL8_SITENAME = "";
+var scContainer = document.querySelector(".claymind-crl8");
+if (scContainer) {
+    CRL8_SITENAME = scContainer.dataset.siteName;
+}
+console.log('CRL8_SITENAME: ' + CRL8_SITENAME);
 !function(){var e=window.crl8=window.crl8||{},n=!1,i=[];e.ready=function(e){n?e():i.push(e)},e.pixel=e.pixel||function(){e.pixel.q.push(arguments)},e.pixel.q=e.pixel.q||[];var t=window.document,o=t.createElement("script"),c=e.debug||-1!==t.location.search.indexOf("crl8-debug=true")?"js":"min.js";o.async=!0,o.src=t.location.protocol+"//edge.curalate.com/sites/"+CRL8_SITENAME+"/site/latest/site."+c,o.onload=function(){n=!0,i.forEach(function(e){e()})};var r=t.getElementsByTagName("script")[0];r.parentNode.insertBefore(o,r.nextSibling)}();
-</script>
